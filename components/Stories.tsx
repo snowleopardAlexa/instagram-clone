0118e8dfd34dacb00614343b1b011ab37795bea2
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-09 21:33:41 -0500

    add scroll faker

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import faker from "@faker-js/faker";
+import Story from "./Story";
 
 const Stories = () => {
+  const [suggestions, setSuggestions] = useState<string[]>([])  
 
   useEffect(() => {
    const suggestions = [...Array(20)].map((_, i) => ({
@@ -9,11 +11,20 @@ const Stories = () => {
      id: i,
    }))
 
-   console.log(suggestions)
+   setSuggestions(suggestions);
   }, [])  
 
   return (
-    <div>Stories</div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll">
+      {suggestions.map(profile => (
+        <Story 
+          key={profile.id}
+          img={profile.avatar} 
+          username={profile.username}
+        />
+      ))}
+
+    </div>
   )
 }
 
