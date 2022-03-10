4f8075190cf1135da0e56d0fab7ebcfc1fc9c932
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-09 21:41:20 -0500

    add scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from "../components/Header";
 import Feed from "../components/Feed";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    <div>
+    <div className="bg-gray-50 h-screen overflow y-scroll scrollbar-hide">
       <Head>
         <title>Instagram Clone</title>
       </Head>
