b4b82e864605378f320b829df1390a1780ff5048
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-09 19:31:30 -0500

    add search

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
     <div> 
       <div className="flex justify-between max-w-6xl">
-        <div className="relative hidden lg:inline-grid h-24 w-24">
+        <div className="relative hidden lg:inline-grid w-24">
         <Image 
           src="/instagram-text-logo.png"
           alt="logo"
@@ -12,13 +13,23 @@ const Header = () => {
           objectFit="contain"
         />
         </div>
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+        <div className="relative w-10 lg:hidden flex-shrink-0">
         <Image 
           src="/instagram-logo.png"
           alt="logo"
           layout="fill"
           objectFit="contain"
         />
+        </div>
+        <div className="relative mt-1 p-3 rounded-md"> 
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input 
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black" 
+              type="text"
+              placeholder="Search" 
+            />
         </div>
       </div>
     </div> 
