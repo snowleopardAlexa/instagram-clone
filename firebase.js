1fff8a6bae7149c50861d597fa13342be27311a8
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-11 20:59:46 -0500

    install firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4anUZU2nCfXEpIf_hqgtRz2fVvzPNZmU",
+  authDomain: "instagram-clone-alexa.firebaseapp.com",
+  projectId: "instagram-clone-alexa",
+  storageBucket: "instagram-clone-alexa.appspot.com",
+  messagingSenderId: "917044577431",
+  appId: "1:917044577431:web:22145ddbae61fde74995d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
