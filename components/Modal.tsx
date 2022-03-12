3b9c31926ea5a2551101f681cff5552113e1e47c
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-12 14:13:38 -0500

    add transition root and transition child

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,16 +1,33 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
+import { Dialog, Transition } from "@headlessui/react";
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState);    
 
   return (
-    <div>
-
-    {open && (
-        <p>THE MODAL IS OPEN</p>
-    )}
-    </div>
+    <Transtion.Root show={open} as={Fragment}>
+       <Dialog
+        as="div"
+        className="fix z-10 inset-0 overflow-y-auto"
+        onClose={setOpen}
+       >
+        <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 
+        text-center sm:block sm:p-0">
+         <Transition.Child
+           as={Fragment}
+           enter="ease-out duration-300"
+           enterFrom="opacity-0"
+           enterTo="opacity-100"
+           leave="ease-in duration-200"
+           leaveFrom="opacity-100"
+           leaveTo="opacity-0"
+         >
+        
+        </Transition.Child>     
+        </div>
+       </Dialog>
+    </Transtion.Root>
   )
 }
 
