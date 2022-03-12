bc9cf98e73582a5868571f1c9807dc8c70e037f7
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-12 14:20:01 -0500

    style modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -23,8 +23,29 @@ const Modal = () => {
            leaveFrom="opacity-100"
            leaveTo="opacity-0"
          >
-        
-        </Transition.Child>     
+          <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />     
+        </Transition.Child>    
+        {/* This element is to trick the browser into centering the modal contents */} 
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        > 
+         &#8203;
+        </span>
+        <Transition.Child
+           as={Fragment}
+           enter="ease-out duration-300"
+           enterFrom="opacity-0"
+           enterTo="opacity-100"
+           leave="ease-in duration-200"
+           leaveFrom="opacity-100"
+           leaveTo="opacity-0"
+         >
+          <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left
+          overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full">
+            <h1>Hello</h1>
+          </div>
+        </Transition.Child>  
         </div>
        </Dialog>
     </Transtion.Root>
