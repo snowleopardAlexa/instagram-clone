1335cf1c5363877ddb14adfec5ed8b2b68ff9a89
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-12 13:27:57 -0500

    add bacticks styles

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -9,18 +9,16 @@ const MiniProfile = () => {
 
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
-        <Image 
-          className="rounded-full border p-[2px]"
-          src="/alexa.jpeg"
+        <img
+          className="w-16 h-16 rounded-full border p-[2px]"
+          src={session?.user?.image}
           alt="profile photo"
-          width="60px"
-          height="60px"
         />
         <div className="flex-1 mx-4">
-            <h2 className="font-bold">alexaalexa</h2>
+            <h2 className="font-bold">{session?.user?.username}</h2>
             <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
         </div>
-        <button className="text-blue-400 text-sm font-semibold">Sign out</button>
+        <button onClick={signOut} className="text-blue-400 text-sm font-semibold">Sign out</button>
     </div>
   )
 }
