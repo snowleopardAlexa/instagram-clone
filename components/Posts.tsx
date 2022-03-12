c00ad98313cac6cc2feced4f419e8d3cba8a3451
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-12 16:12:10 -0500

    add data() to map posts

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -40,10 +40,10 @@ const Posts = () => {
        <Post 
          key={post.id} 
          id={post.id}
-         username={post.username}
-         userImg={post.userImg}
-         img={post.img}
-         caption={post.caption}
+         username={post.data().username}
+         userImg={post.data().profileImg}
+         img={post.data().img}
+         caption={post.data().caption}
          />
       ))}
      
