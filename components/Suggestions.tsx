1ef04577a0e2a574dbded1d1a78cdbce0b7c672c
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-11 20:33:05 -0500

    add suggestions

--- components/Suggestions.tsx
+++ components/Suggestions.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import faker from "@faker-js/faker";
+
 const Suggestions = () => {
+
+const [suggestions, setSuggestions] = useState([]);
+
+useEffect(() => {
+  const suggestions = [...Array(5)].map((_, i) => ({
+    ...faker.helpers.contextualCard(),
+    id: i,
+  }))
+
+  setSuggestions(suggestions)
+}, [])
+
   return (
-    <div></div>
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See All</button>
+      </div>
+    </div>
   )
 }
 
