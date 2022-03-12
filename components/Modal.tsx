12cf272cf1850a28301fbc45ea9fc96607417469
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-12 14:39:39 -0500

    add btn upload with styles

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,16 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
 import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState);    
 
   return (
-    <Transtion.Root show={open} as={Fragment}>
+    <Transition.Root show={open} as={Fragment}>
        <Dialog
         as="div"
-        className="fix z-10 inset-0 overflow-y-auto"
+        className="fixed z-10 inset-0 overflow-y-auto"
         onClose={setOpen}
        >
         <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 
@@ -42,13 +43,26 @@ const Modal = () => {
            leaveTo="opacity-0"
          >
           <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left
-          overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full">
-            <h1>Hello</h1>
-          </div>
+          overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+           <div>
+            <div className="mt-5 sm:mt-6">
+
+                <button
+                  type="button"
+                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm 
+                  px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
+                  focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300
+                  disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                >
+                  Upload Post
+                </button>
+            </div>
+           </div>
+           </div>
         </Transition.Child>  
         </div>
        </Dialog>
-    </Transtion.Root>
+    </Transition.Root>
   )
 }
 
