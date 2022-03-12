391bfe50d36c666f62e92318f2017ef75d7bfabf
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-11 19:15:21 -0500

    change to image next

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,62 +1,72 @@
-import Image from "next/image";
-import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon } from "@heroicons/react/outline";
-import { HomeIcon } from "@heroicons/react/solid";
-
+import Image from 'next/image'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid';
 
 const Header = () => {
   return (
-    <div className="shadow-sm border-b bg-white sticky top-0 z-50"> 
-      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
-        <div className="relative hidden lg:inline-grid w-24">
-        <Image 
-          src="/instagram-text-logo.png"
-          alt="logo"
-          layout="fill"
-          objectFit="contain"
-        />
+    <div className="sticky top-0 z-50 border-b bg-white shadow-sm">
+      <div className="mx-5 flex max-w-6xl justify-between lg:mx-auto">
+        <div className="relative hidden w-24 lg:inline-grid">
+          <Image
+            src="/instagram-text-logo.png"
+            alt="logo"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
-        <div className="relative w-10 lg:hidden flex-shrink-0">
-        <Image 
-          src="/instagram-logo.png"
-          alt="logo"
-          layout="fill"
-          objectFit="contain"
-        />
+        <div className="relative w-10 flex-shrink-0 lg:hidden">
+          <Image
+            src="/instagram-logo.png"
+            alt="logo"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
         <div className="max-w-xs">
-        <div className="relative mt-1 p-3 rounded-md"> 
-            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                <SearchIcon className="h-5 w-5 text-gray-500" />
+          <div className="relative mt-1 rounded-md p-3">
+            <div className="pointer-events-none absolute inset-y-0 flex items-center pl-3">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
             </div>
-            <input 
-              className="bg-gray-50 block w-full pl-10 sm:text-sm 
-              border-gray focus:ring-black focus:border-black rounded-md" 
+            <input
+              className="border-gray block w-full rounded-md bg-gray-50 
+              pl-10 focus:border-black focus:ring-black sm:text-sm"
               type="text"
-              placeholder="Search" 
+              placeholder="Search"
             />
-        </div>
+          </div>
         </div>
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn" />
-          <MenuIcon className="h-6 md:hidden cursor-pointer" />
-          <div className="relative navBtn">
-           <PaperAirplaneIcon className="navBtn rotate-45" />
-           <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 
-           rounded-full flex items-center justify-center animate-pulse text-white">7</div>
+          <MenuIcon className="h-6 cursor-pointer md:hidden" />
+          <div className="navBtn relative">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div
+              className="absolute -top-1 -right-2 flex h-5 w-5 animate-pulse 
+           items-center justify-center rounded-full bg-red-500 text-xs text-white"
+            >
+              7
+            </div>
           </div>
           <PlusCircleIcon className="navBtn" />
           <UserGroupIcon className="navBtn" />
           <HeartIcon className="navBtn" />
-          <Image 
+          <Image
             className="imgD"
             src="/alexa.jpeg"
             alt="profile photo"
-            width="35"
-            height="40"
+            width="40px"
+            height="40px"
           />
         </div>
       </div>
-    </div> 
+    </div>
   )
 }
 
