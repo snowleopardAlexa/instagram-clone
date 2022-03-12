2c6b0048b14b7d2897d11d2f2dec0c36faed6a93
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-12 14:42:37 -0500

    add div upload a photo

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -45,8 +45,15 @@ const Modal = () => {
           <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left
           overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
            <div>
+            <div className="mt-3 text-center sm:mt-5">
+               <Dialog.Title
+                 as="h3"
+                 className="text-lg leading-6 font-medium text-gray-900"
+               >
+                 Upload a photo   
+               </Dialog.Title>
+            </div>
             <div className="mt-5 sm:mt-6">
-
                 <button
                   type="button"
                   className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm 
