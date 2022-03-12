83fffcec7841b16cd350bbee41947c49f31de025
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-11 19:54:32 -0500

    add flex and padding

--- components/Post.tsx
+++ components/Post.tsx
@@ -26,10 +26,13 @@ const Post = ({ id, username, userImg, img, caption }) => {
           width="800px"
           height="500px"
         />
+        <div className="flex justify-between px-4 pt-4">
         <div className="flex space-x-4">
             <HeartIcon className="btn" />
             <ChatIcon className="btn" />
             <PaperAirplaneIcon className="btn" />
+        </div>
+            <BookmarkIcon className="btn" />
         </div>
     </div>
   )
