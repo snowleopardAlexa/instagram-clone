a9a44956b818bcdf2662b767f0ded34a59af049c
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-11 21:42:26 -0500

    change next auth url to localhost

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
 
-const signIn = () => {
+// runs in the browser 
+const signIn = ({ providers }) => {
   return (
-    <div>I sign in page</div>
+   <>
+     {Object.values(providers).map((provider) => (
+         <div key={provider.name}>
+             <button onClick={() => SignIntoProvider(provider.id)}>
+                 Sign in with {provider.name}
+             </button>
+         </div>
+     ))}
+   </>
   )
+}
+
+// runs on middle server - server side rendering 
+export async function getServerSideProps() {
+  const providers = await getProviders()
+
+  return {
+      props: {
+          providers,
+      },
+   }
 }
 
 export default signIn
